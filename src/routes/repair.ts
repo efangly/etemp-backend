50154c23492474392c0f5bdec0856ea6c65b6353
efangly <90622250+efangly@users.noreply.github.com>
2024-02-13 10:41:17 +0700

    edit repair route

--- src/routes/repair.ts
+++ src/routes/repair.ts
@@ -5,9 +5,9 @@ const RepairRouter: Router = Router();
 
 //user 
 RepairRouter.get('/', ...requireLogin(), verifyToken, repair.getRepair);
-RepairRouter.get('/:repairid', ...requireLogin(), verifyToken, repair.getRepairById);
+RepairRouter.get('/:repair_id', ...requireLogin(), verifyToken, repair.getRepairById);
 RepairRouter.post('/', ...requireLogin(), repair.createRepair);
-RepairRouter.put('/:repairid', ...requireLogin(), repair.updateRepair);
-RepairRouter.delete('/:repairid', ...requireLogin(), repair.deleteRepair);
+RepairRouter.put('/:repair_id', ...requireLogin(), repair.updateRepair);
+RepairRouter.delete('/:repair_id', ...requireLogin(), repair.deleteRepair);
 
 export default RepairRouter;
